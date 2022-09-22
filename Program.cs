c69abccf59d29c85a0573c7c7354da179d71c548
Malek Ghieth <malek.ghieth@aerdata.com>
2022-09-23 00:48:51 +0300

    firstcommit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace BestPrediction
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("first commit");
         }
     }
 }

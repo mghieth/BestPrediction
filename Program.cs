9666a5c17c3f4280bc4de08dab9c217af61de51b
Malek Ghieth <malek.ghieth@aerdata.com>
2022-09-23 00:47:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestPrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

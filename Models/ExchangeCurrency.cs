b23622f6527c494e21e66bca6c12b0ce3334b635
Malek Ghieth <malek.ghieth@aerdata.com>
2022-09-25 09:58:17 +0300

    Add two models

---
+++ Models/ExchangeCurrency.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestPrediction.Models
+{
+    public class ExchangeCurrency
+    {
+        public int Id { get; set; }
+        public decimal ExchangeRate { get; set; }
+        public DateTime ExchangeDate { get; set; }
+        public Currency Currency { get; set; }
+    }
+}
